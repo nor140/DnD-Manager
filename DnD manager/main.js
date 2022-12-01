c38e21721f1cac1c894a6585d8a8c0e438a878d9
nor140 <51517081+nor140@users.noreply.github.com>
2022-12-02 00:12:36 +0300

    Delete DnD manager directory

--- DnD manager/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    document.querySelector('.character-wrapper').addEventListener('click', function(){
-        document.querySelector('.characters').insertAdjacentHTML('afterbegin', `
-        <div class="character-wrapper">
-            <input class='name' type="text" name="input">
-        </div>
-        `);
-    });
- }
