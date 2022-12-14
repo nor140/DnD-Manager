1cbec23131b42a31ad861946d73229341ea01ca6
nor140 <51517081+nor140@users.noreply.github.com>
2022-12-14 18:52:32 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
+
 window.onload = function() {
     document.querySelector('.character-wrapper').addEventListener('click', function(){
         document.querySelector('.characters').insertAdjacentHTML('afterbegin', `
         <div class="character-wrapper">
-            <input class='name' type="text" name="input">
-        </div>
+        <p class="name-raw">имя</p>
+            <div  contenteditable="true" data-placeholder="Введите текст"></div>
+        <p class="name-raw">Внешность</p>
+        <div  contenteditable="true" data-placeholder="Введите текст"></div>
+        <p class="name-raw">Характер</p>
+        <div  contenteditable="true" data-placeholder='Введите текст'></div>
+        <p class="name-raw">Предыстория</p>
+        <div  contenteditable="true" data-placeholder='Введите текст'></div>
+    </div>
         `);
     });
- }
+    
+}   
+function upload(event){
+    console.log(event);
+    event.target.closest('.parent').querySelector('.child').insertAdjacentHTML('afterbegin', `
+    <div class="parent">
+        <div class="start"></div>
+        <div class="location" >
+            <div class="loc-text" contenteditable="true" data-placeholder="событие"></div>
+        </div>
+        <div class="line"></div>
+        <div class="child">
+            
+            <div class="location-plus" >
+                <img src="svg/plus-svgrepo-com.svg" alt="plus" class="loc-plus-img" onclick="upload(event)">
+            </div>
+        </div>
+    </div>
+    `);
+}
